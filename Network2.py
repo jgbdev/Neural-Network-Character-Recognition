d08254e01f9ad9525c4f2cfff8881adf5c591130
John <john.griffith@live.co.uk>
2017-01-26 22:51:08 +0000

    wip

--- Network2.py
+++ Network2.py
@@ -1,5 +1,42 @@
 import theano
+import theano.tensor as T
 import numpy as np
+
+
+
+class Network(object):
+
+    def init(self, layers, mini_batch_size):
+        self.layers = layers
+        self.mini_batch_size = mini_batch_size
+        self.params = [param for layers in self.layers for param in layers.params]
+        self.x = T.matrix("x")
+        self.y = T.ivector("y")
+        init_layer = self.layers[0]
+        init_layer.set_inpt(self.x, self.x, self.mini_batch_size)
+        for j in range(1, len(self.layers)):
+            prev_layer, layer = self.layers[j-1], self.layers[j]
+            layer.set_inpt(
+                prev_layer.output, prev_layer.output_dropout, self.mini_batch_size)
+        self.output = self.layers[-1].output
+        self.output_dropout = self.layers[-1].output_dropout
+
+
+    def SGD(self, training_data, spochs, mini_batch_size, eta, validataion_data,
+            test_data, lmbda=0.0):
+        training_x , training_y = training_data
+        validation_x, validation_y = validataion_data
+        test_x, test_y = test_data
+
+        #Compute mini batch sizes
+        num_training_batches = size(training_data)/mini_batch_size
+        num_validation_batches = size(training_data)
+
+        l2_normal_squared = sum([(layer.w**2).sum() for layer in self.layers])
+        cost = self.layers[-1].cost(self)+\
+            0.5*lmbda*l2_normal_squared/num_training_batches
+        grads = T.grad(cost,self.params)
+
 
 
 class FullyConnectedLayer(object):
@@ -22,14 +59,16 @@ class FullyConnectedLayer(object):
             name='b', borrow=True)
         self.params = [self.w, self.b]
 
-
-
-    def set_inpt(self, inpt, inpt_dropout, minibatch_size):
-        self.inpt = inpt.reshape((minibatch_size, self.n_in))
+    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+        self.inpt = inpt.reshape((mini_batch_size, self.n_in))
         self.output = self.activation_fn(
-            (1-self.p_dropout)*T.dot(self.inpt, self.w) + self.b)
-        )
+            (1 - self.p_dropout) * T.dot(self.inpt, self.w) + self.b)
         self.y_out = T.argmax(self.output, axis=1)
-        self.inpt_dropout =
+        self.inpt_dropout = dropout_layer(
+            inpt_dropout.reshape((mini_batch_size, self.n_in)), self.p_dropout)
+        self.output_dropout = self.activation_fn(
+            T.dot(self.inpt_dropout, self.w) + self.b)
+
+
 
 
