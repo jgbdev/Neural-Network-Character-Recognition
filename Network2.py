6be0fa7f56e85e05038e7e0dbf3c9fc89f5f91ad
John <john.griffith@live.co.uk>
2017-01-27 22:02:17 +0000

    Base working

--- Network2.py
+++ Network2.py
@@ -1,12 +1,49 @@
 import theano
 import theano.tensor as T
-import numpy as np
+from theano.tensor import shared_randomstreams
+from theano.tensor.nnet import sigmoid
+from theano.tensor import tanh
+from theano.tensor.nnet import conv
+from theano.tensor.nnet import softmax
 
+import numpy as np
+import cPickle
+import gzip
+
+
+#### Constants
+GPU = True
+
+if GPU:
+    print "Trying to run under a GPU.  If this is not desired, then modify "+\
+        "network3.py\nto set the GPU flag to False."
+    try: theano.config.device = 'gpu'
+    except: pass # it's already set
+    theano.config.floatX = 'float32'
+else:
+    print "Running with a CPU.  If this is not desired, then the modify "+\
+        "network3.py to set\nthe GPU flag to True."
+
+def load_data_shared(filename="./mnist.pkl.gz"):
+    f = gzip.open(filename, 'rb')
+    training_data, validation_data, test_data = cPickle.load(f)
+    f.close()
+    def shared(data):
+        """Place the data into shared variables.  This allows Theano to copy
+        the data to the GPU, if one is available.
+
+        """
+        shared_x = theano.shared(
+            np.asarray(data[0], dtype=theano.config.floatX), borrow=True)
+        shared_y = theano.shared(
+            np.asarray(data[1], dtype=theano.config.floatX), borrow=True)
+        return shared_x, T.cast(shared_y, "int32")
+    return [shared(training_data), shared(validation_data), shared(test_data)]
 
 
 class Network(object):
 
-    def init(self, layers, mini_batch_size):
+    def __init__(self, layers, mini_batch_size):
         self.layers = layers
         self.mini_batch_size = mini_batch_size
         self.params = [param for layers in self.layers for param in layers.params]
@@ -22,7 +59,7 @@ class Network(object):
         self.output_dropout = self.layers[-1].output_dropout
 
 
-    def SGD(self, training_data, spochs, mini_batch_size, eta, validataion_data,
+    def SGD(self, training_data, epochs, mini_batch_size, eta, validataion_data,
             test_data, lmbda=0.0):
         training_x , training_y = training_data
         validation_x, validation_y = validataion_data
@@ -30,18 +67,80 @@ class Network(object):
 
         #Compute mini batch sizes
         num_training_batches = size(training_data)/mini_batch_size
-        num_validation_batches = size(training_data)
+        num_validation_batches = size(validataion_data)/mini_batch_size
+        num_test_batches = size(test_data)/mini_batch_size
 
         l2_normal_squared = sum([(layer.w**2).sum() for layer in self.layers])
         cost = self.layers[-1].cost(self)+\
             0.5*lmbda*l2_normal_squared/num_training_batches
         grads = T.grad(cost,self.params)
+        updates = [(param, param-eta*grad)
+                   for param, grad in zip(self.params,grads)]
 
+        i = T.lscalar()
+        train_mb = theano.function(
+            [i], cost, updates=updates,
+               givens={self.x:
+                       training_x[i*self.mini_batch_size: (i+1)*self.mini_batch_size],
+                       self.y:
+                       training_y[i*self.mini_batch_size: (i+1)*self.mini_batch_size]})
+
+        validate_mb_accuracy = theano.function(
+            [i], self.layers[-1].accuracy(self.y),
+            givens={
+                self.x:
+                    validation_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
+                self.y:
+                    validation_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
+            })
+        test_mb_accuracy = theano.function(
+            [i], self.layers[-1].accuracy(self.y),
+            givens={
+                self.x:
+                    test_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
+                self.y:
+                    test_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
+            })
+
+
+        self.test_mb_predictions = theano.function(
+            [i], self.layers[-1].y_out,
+            givens={
+                self.x:
+                    test_x[i*self.mini_batch_size: (i+1)*self.mini_batch_size]
+            })
+
+        best_validation_accuracy = 0.0
+
+        for epoch in xrange(epochs):
+            for minibatch_index in xrange(num_training_batches):
+                iteration = num_training_batches*epoch + minibatch_index
+                if iteration % 1000 == 0:
+                    print ("Training mini-batch number {0}".format(iteration))
+                cost_ij = train_mb(minibatch_index)
+                if(iteration+1) % num_training_batches == 0:
+                    validation_accuracy = np.mean(
+                        [validate_mb_accuracy(j) for j in xrange(num_validation_batches)])
+                    print("Epoch {0}: validataion accuracy {1:.2}".format(epoch, validation_accuracy))
+
+                    if validation_accuracy > best_validation_accuracy:
+                        print "This is the best validation accuracy to date"
+                        best_validation_accuracy = validation_accuracy
+                        best_iteration = iteration
+
+                        if test_data:
+                            test_accuracy = np.mean(
+                                [test_mb_accuracy(j) for j in xrange(num_test_batches)])
+                            print ('The corresponding test accuracy is {0:.2}'.format(test_accuracy))
+
+        print ("Finish training network.")
+        print ("Best validation accuracy of {0:.2} at {1} iteration".format(best_validation_accuracy, best_iteration))
+        print ("Corresponding test accuracy of {0:.2}".format(test_accuracy))
 
 
 class FullyConnectedLayer(object):
 
-    def __init__(self, n_in, n_out, activation_fn=2, p_dropout=0.0):
+    def __init__(self, n_in, n_out, activation_fn=sigmoid, p_dropout=0.0):
         self.n_in = n_in
         self.n_out = n_out
         self.activation_fn = activation_fn
@@ -69,6 +168,50 @@ class FullyConnectedLayer(object):
         self.output_dropout = self.activation_fn(
             T.dot(self.inpt_dropout, self.w) + self.b)
 
+    def accuracy(self, y):
+        "Return the accuracy for the mini-batch."
+        return T.mean(T.eq(y, self.y_out))
 
 
+class SoftmaxLayer(object):
+
+    def __init__(self, n_in, n_out, p_dropout=0.0):
+        self.n_in = n_in
+        self.n_out = n_out
+        self.p_dropout = p_dropout
+        # Initialize weights and biases
+        self.w = theano.shared(
+            np.zeros((n_in, n_out), dtype=theano.config.floatX),
+            name='w', borrow=True)
+        self.b = theano.shared(
+            np.zeros((n_out,), dtype=theano.config.floatX),
+            name='b', borrow=True)
+        self.params = [self.w, self.b]
+
+    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+        self.inpt = inpt.reshape((mini_batch_size, self.n_in))
+        self.output = softmax((1-self.p_dropout)*T.dot(self.inpt, self.w) + self.b)
+        self.y_out = T.argmax(self.output, axis=1)
+        self.inpt_dropout = dropout_layer(
+            inpt_dropout.reshape((mini_batch_size, self.n_in)), self.p_dropout)
+        self.output_dropout = softmax(T.dot(self.inpt_dropout, self.w) + self.b)
+
+    def cost(self, net):
+        "Return the log-likelihood cost."
+        return -T.mean(T.log(self.output_dropout)[T.arange(net.y.shape[0]), net.y])
+
+    def accuracy(self, y):
+        "Return the accuracy for the mini-batch."
+        return T.mean(T.eq(y, self.y_out))
+
+
+def size(data):
+    "Return the size of the dataset `data`."
+    return data[0].get_value(borrow=True).shape[0]
+
+def dropout_layer(layer, p_dropout):
+    srng = shared_randomstreams.RandomStreams(
+        np.random.RandomState(0).randint(999999))
+    mask = srng.binomial(n=1, p=1-p_dropout, size=layer.shape)
+    return layer*T.cast(mask, theano.config.floatX)
 
--- Run.py
+++ Run.py
@@ -1,6 +1,5 @@
 import Network as n1
 import Network2 as n2
-import mminst_loader
 import timeit
 
 
@@ -8,15 +7,17 @@ import timeit
 
 def main():
 
-    net2 = n2.FullyConnectedLayer(200,100)
 
-    net = n1.Network([784, 30, 10], cost=n1.Network.CrossEntropy, debug=True)
 
-    training_data, validation_data, test_data = mminst_loader.load_data_wrapper()
-
+    training_data, validation_data, test_data = n2.load_data_shared()
+    mini_batch_size = 10
+    # net = n1.Network([784, 30, 10], cost=n1.Network.CrossEntropy, debug=True)
+    net = n2.Network([n2.FullyConnectedLayer(784, 100),
+                      n2.SoftmaxLayer(n_in=100,n_out=10)], mini_batch_size)
     print "Starting"
     start_time = timeit.default_timer()
-    net.SGD(training_data, 30, 10, 0.1, decay=5.0, test_data=test_data, early_stop=10)
+    net.SGD(training_data,60, mini_batch_size, 0.01, validation_data, test_data, lmbda=5.0)
+    #net.SGD(training_data, 30, 10, 0.1, decay=5.0, test_data=test_data, early_stop=10)
     elapsed = timeit.default_timer() - start_time
     print "Elapsed time " + str(elapsed)
 
