378fca8d9798b372fd84ac58bd4e3cc448b7e5bf
John <john.griffith@live.co.uk>
2017-01-28 11:40:16 +0000

    Adding best accuracy to Run

--- Run.py
+++ Run.py
@@ -22,13 +22,14 @@ def main():
               n2.FullyConnectedLayer(n_in=100, n_out=100),
               n2.SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 
+    '''
     net = n2.Network([
         n2.ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                       filter_shape=(20, 1, 5, 5),
                       poolsize=(2, 2)),
         n2.FullyConnectedLayer(n_in=20 * 12 * 12, n_out=100),
         n2.SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
-
+    '''
     print "Starting"
     start_time = timeit.default_timer()
     net.SGD(training_data,60, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
