f6dd27a4669d2e487ded319979e6401c23c53c89
John Griffith <john.griffith@live.co.uk>
2017-03-25 10:35:52 +0000

    Updated pool api

--- Network2.py
+++ Network2.py
@@ -5,7 +5,7 @@ from theano.tensor.nnet import sigmoid
 from theano.tensor import tanh
 from theano.tensor.nnet import conv
 from theano.tensor.nnet import softmax
-from theano.tensor.signal import downsample
+from theano.tensor.signal import pool
 import numpy as np
 import cPickle
 import gzip
@@ -229,7 +229,7 @@ class ConvPoolLayer(object):
         conv_out = conv.conv2d(
             input=self.inpt, filters=self.w, filter_shape=self.filter_shape,
             image_shape=self.image_shape)
-        pooled_out = downsample.max_pool_2d(
+        pooled_out = pool.pool_2d(
             input=conv_out, ds = self.poolsize, ignore_border=True)
         self.output = self.activation_fn(pooled_out + self.b.dimshuffle('x',0,'x','x'))
         self.output_dropout = self.output
