a29bf6bb519c39f2071d06477f1d73eaeea5c0ae
John Griffith <john.griffith@live.co.uk>
2017-03-25 12:27:34 +0000

    Updated gpu test

--- Run.py
+++ Run.py
@@ -1,8 +1,9 @@
 import Network as n1
 import Network2 as n2
 import timeit
-from theano import function, config, shared, sandbox
-import theano.tensor as T
+
+from theano import function, config, shared, tensor
+
 import numpy
 import time
 
@@ -15,7 +16,7 @@ def checkGPU():
 
     rng = numpy.random.RandomState(22)
     x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
-    f = function([], T.exp(x))
+    f = function([], tensor.exp(x))
     print(f.maker.fgraph.toposort())
     t0 = time.time()
     for i in range(iters):
@@ -23,7 +24,9 @@ def checkGPU():
     t1 = time.time()
     print("Looping %d times took %f seconds" % (iters, t1 - t0))
     print("Result is %s" % (r,))
-    if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
+    if numpy.any([isinstance(x.op, tensor.Elemwise) and
+                          ('Gpu' not in type(x.op).__name__)
+                  for x in f.maker.fgraph.toposort()]):
         print('Used the cpu')
     else:
         print('Used the gpu')
